ce3e466c35bce78eae435d0e53623c531faa6f94
Fer Perales <fer.perales.montoya@gmail.com>
2013-08-08 18:46:23 -0500

    Add solution

---
+++ solucion.rb
@@ -0,0 +1,47 @@
+class Dessert
+	attr_accessor :name, :calories
+
+	def initialize(name, calories)
+		@name = name
+		@calories = calories
+	end
+
+	def healthy?
+		@calories<200
+	end
+
+	def delicious?
+		true
+	end
+end
+
+class JellyBean < Dessert
+
+	attr_accessor :flavor
+	
+	def initialize(name, calories,flavor)
+		super(name,calories)
+		@flavor=flavor
+	end
+
+	def delicious?
+		flavor!="black licorice"
+	end
+end
+
+dessert1=Dessert.new("icecream",400)
+dessert2=JellyBean.new("icecream",400,"black licorice")
+puts "**************************************************"
+puts "Datos del postre 1:"
+puts "Nombre: #{dessert1.name}"
+puts "Calorias: #{dessert1.calories}"
+puts "Es saludable?: #{dessert1.healthy?}"
+puts "Es delicioso? #{dessert2.delicious?}"
+puts "**************************************************"
+puts "Datos del postre 2:"
+puts "Nombre: #{dessert2.name}"
+puts "Calorias: #{dessert2.calories}"
+puts "Sabor: #{dessert2.flavor}"
+puts "Es saludable?: #{dessert2.healthy?}"
+puts "Es delicioso? #{dessert2.delicious?}"
+puts "**************************************************"
